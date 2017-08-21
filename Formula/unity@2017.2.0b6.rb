ffc9a2dc071853eb7a77888f32e2f31ea77020e3
Rodrigo Russo <rodrigozrusso@gmail.com>
2017-08-22 01:33:59 +0200

    testing brew unity

---
+++ Formula/unity@2017.2.0b6.rb
@@ -0,0 +1,38 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook.html
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class UnityAT201720b6 < Formula
+  desc ""
+  homepage ""
+  url "http://beta.unity3d.com/download/b6bf3c071fe7/MacEditorInstaller/Unity-2017.2.0b6.pkg"
+  sha256 "ba7ac3ed7cb1e9ebeaa55bfc48881d6a99cec21d5d05fa5631399337416ce7bb"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    system "installer", "-dumplog", "-package", "Unity.pkg", "-target", "/"
+    # system "./configure", "--disable-debug",
+    #                       "--disable-dependency-tracking",
+    #                       "--disable-silent-rules",
+    #                       "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    # system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test Unity`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
